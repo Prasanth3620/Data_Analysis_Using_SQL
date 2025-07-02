6e6055e39e83c01a3fe100a000d7a0f1e4f11055
Prasanth3620 <reddiprasanth1808@gmail.com>
2025-07-02 10:34:52 +0530

    Update 01_dimensions_exploration.sql

--- 01_dimensions_exploration.sql
+++ 01_dimensions_exploration.sql
@@ -1,15 +1,13 @@
 
 /*
-===============================================================================
+
 Dimensions Exploration
-===============================================================================
 Purpose:
     - To explore the structure of dimension tables.
 	
 SQL Functions Used:
     - DISTINCT
     - ORDER BY
-===============================================================================
 */
 
 -- Retrieve a list of unique countries from which customers originate
